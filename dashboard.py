f3aa43c56579444d3b596e2481c9c99711e13d12
RobinLoxlyHood <iki11111998@gmail.com>
2023-03-28 04:19:41 +0700

    add line chart

--- dashboard.py
+++ dashboard.py
@@ -5,8 +5,8 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import geopandas as gpd
-import matplotlib.pyplot as plt
-
+import plotly.graph_objects as go
+import plotly.express as px
 from PIL import Image
 import warnings
 warnings.filterwarnings('ignore')
@@ -51,7 +51,7 @@ def filters_tokoh(df_jkt):
 
 def filter_sentiment_positif_tiap_provinsi(df):
     location_unique = df['location'].unique()
-    new_df = gpd.GeoDataFrame()
+    new_df = pd.DataFrame()
     for location in location_unique:
         location_sentiment = df[(df['location'] == location) & (df['Sentiment'] == '1')]
         highest_jumlah = location_sentiment.loc[location_sentiment['jumlah'].idxmax()]  # Filter for the highest 'jumlah' value
@@ -129,17 +129,54 @@ def display_map(df_jkt):
 def plot_tokoh_sentiment(df, tokoh):
     # Mengelompokkan berdasarkan tokoh dan sentiment, kemudian menghitung jumlah
     result = df.groupby(['Tokoh', 'Sentiment'])['jumlah'].sum().reset_index()
-
+    
+    # Mengubah label sentimen
+    result['Sentiment'] = result['Sentiment'].apply(lambda x: 'Netral' if x == '-1' else 'Negatif' if x == '0' else 'Positif')
+    
     # Membuat barchart untuk tokoh yang dimaksud
     df_tokoh = result[result['Tokoh'] == tokoh]
     fig = go.Figure(
-        data=[go.Bar(x=df_tokoh['Sentiment'], y=df_tokoh['jumlah'])],
-        layout=go.Layout(title=tokoh)
+        data=[go.Bar(
+            x=df_tokoh['Sentiment'],
+            y=df_tokoh['jumlah'],
+            marker=dict(color=['red' if s == 'Negatif' else 'gray' if s == 'Netral' else 'green' for s in df_tokoh['Sentiment']])
+        )],
+        layout=go.Layout(title='Jumlah Sentiment keseluruhan Provinsi')
     )
+    
+    
+    # Menambahkan anotasi di atas bar
+    for i, row in df_tokoh.iterrows():
+        fig.add_annotation(
+            x=row['Sentiment'],
+            y=row['jumlah'],
+            text=str(row['jumlah']),
+            font=dict(color='white'),
+            showarrow=False
+        )
+    fig.update_layout(width=300, height=450)
+    
     st.plotly_chart(fig)
-
-
-
+    
+def create_line_chart(df, tokoh):
+    # Filter data berdasarkan tokoh yang dipilih
+    df_tokoh = df[df['Tokoh'] == tokoh]
+    
+    # Mengubah label sentimen
+    df_tokoh['Sentiment'] = df_tokoh['Sentiment'].apply(lambda x: 'Netral' if x == '-1' else 'Negatif' if x == '0' else 'Positif')
+    
+    # Membuat line chart dengan Plotly
+    fig = px.line(df_tokoh, x='date', y='jumlah', color='Sentiment', 
+                  title='History Jumlah Sentiment Keseluruhan Provinsi', labels={'Sentiment': 'Sentiment Score'})
+    
+    # Menampilkan legend
+    fig.update_traces(mode='lines+markers')
+    
+    # Mengatur ukuran figure
+    fig.update_layout(width=450, height=450)
+    
+    # Menampilkan chart
+    st.plotly_chart(fig)
 
 def main():
     #TITLE
@@ -148,9 +185,10 @@ def main():
     st.title(APP_TITLE)
     #READ MYSQSL DATA
     query = "SELECT * FROM hasil_scraping"
-    
     #READ DATA
     data_df = read_mysql_table(query)
+    # # data_df.to_csv("data.csv", index=False)
+    #   data_df = pd.read_csv("data.csv")
     geo_df = gpd.read_file('indonesia-prov.geojson')
     #mengganti value Status
     replacement_mapping_dict = {
@@ -165,9 +203,8 @@ def main():
     #MERGE
     merged_gdf = geo_df.merge(data_df, on='location')
     merged_gdf = gpd.GeoDataFrame(merged_gdf, geometry='geometry')
-    
+    #Data Preprocessing Barchart
     count_sentiment_tokoh_loc = merged_gdf.groupby(['location' ,'Sentiment', 'Tokoh'])['location'].count().reset_index(name="jumlah")
-    
     merge2=geo_df.merge(count_sentiment_tokoh_loc, on='location')
     merge2 = merge2[['location', 'Tokoh', 'Sentiment', 'jumlah']]
     fil=filter_sentiment_positif_tiap_provinsi(merge2)
@@ -175,6 +212,11 @@ def main():
     fil = gpd.GeoDataFrame(fil, geometry='geometry')
     fil = fil[['location', 'Tokoh', 'Sentiment', 'jumlah', 'geometry']]
     
+    #Data Preprocessing Line Chart
+    #count_sentiment_tokoh_byDate_and_location = merged_gdf.groupby(['date' ,'location' ,'Sentiment', 'Tokoh'])['date'].count().reset_index(name="jumlah")
+    #data_filter_linechart_with_location=count_sentiment_tokoh_byDate_and_location[['date', 'location', 'Tokoh', 'Sentiment', 'jumlah']]
+    count_sentiment_tokoh_byDate = merged_gdf.groupby(['date','Sentiment', 'Tokoh'])['date'].count().reset_index(name="jumlah")
+    data_filter_linechart_byDate=count_sentiment_tokoh_byDate[['date', 'Tokoh', 'Sentiment', 'jumlah']]
     # FILTER  LOKASI
     # lokasi=filters_lokasi(df_jkt=merged_gdf)
     # tokoh =filters_tokoh(df_jkt=lokasi)
@@ -189,9 +231,10 @@ def main():
     #     submitted1 = st.form_submit_button(label = 'Search Twitter 🔎')
     #st.write(merged_gdf)
     #display_map(gdf)
+    st.markdown(f'<h3 style="color: gray;border-radius:50%;" >sumber: www.twitter.com</h3>',unsafe_allow_html=True)
+    st.metric(label="Akurasi", value='75 %')
+    # Using "with" notation
     with st.container():
-         st.markdown(f'<h3 style="color: gray;border-radius:50%;" >sumber: www.twitter.com</h3>',unsafe_allow_html=True)
-         st.metric(label="Akurasi", value='75 %')
          ganjar, anies, prabowo = st.columns(3)
          with ganjar:
             st.subheader("Ganjar Pranowo")
@@ -219,19 +262,30 @@ def main():
     with st.container():
         st.subheader("Jumlah Sentiment Positif terbanyak tiap Provinsi")
         display_map(fil)
-        plot_tokoh_sentiment(count_sentiment_tokoh_loc, 'Ganjar Pranowo')
-        
-        # col1, col2=st.columns(2)
-        # with col1:
-        #     plot_sentiment_by_tokoh(count_sentiment_tokoh_loc, 'Ganjar Pranowo')
-        # with col2:
-        #     plot_sentiment_by_tokoh(count_sentiment_tokoh_loc, 'Anies Baswedan')
-        
-                
-        
+    with st.container():
+        st.subheader("Ganjar Pranowo")
+        barchart, linechart= st.columns(2)
+        with barchart:
+            plot_tokoh_sentiment(count_sentiment_tokoh_loc, 'Ganjar Pranowo')
+        with linechart:
+            create_line_chart(data_filter_linechart_byDate, 'Ganjar Pranowo')
     
-    #plot_tweet_count(merge2)
-    st.write(print(count_sentiment_tokoh_loc))
+    with st.container():
+        st.subheader('Anies Baswedan')
+        barchart, linechart= st.columns(2)
+        with barchart:
+            plot_tokoh_sentiment(count_sentiment_tokoh_loc, 'Anies Baswedan')
+        with linechart:
+            create_line_chart(data_filter_linechart_byDate, 'Anies Baswedan')
+    
+    with st.container():
+        st.subheader('Prabowo Subianto')
+        barchart, linechart= st.columns(2)
+        with barchart:
+            plot_tokoh_sentiment(count_sentiment_tokoh_loc, 'Prabowo Subianto')
+        with linechart:
+            create_line_chart(data_filter_linechart_byDate, 'Prabowo Subianto')
+    # st.write(data_df)
     
 if __name__ == "__main__":
     main()
